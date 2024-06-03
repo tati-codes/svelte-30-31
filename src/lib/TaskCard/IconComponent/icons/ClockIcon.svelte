38c297a379c6ba1b9518c5d0a5f8a847907dee44
Tati <iced.tati@gmail.com>
2024-06-03 15:36:47 -0400

    timerspan component

---
+++ src/lib/TaskCard/IconComponent/icons/ClockIcon.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  export let x = 55, y = 55, hue="medium";
+
+</script>
+    <svg
+      viewBox="0 0 100 412"
+      x={x}
+      y={y}
+      width="80"
+      height="80"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g transform="scale(12), translate(-105, -104)">
+        <circle
+          class={`stubbornClock ${hue}-stroke sT`}
+          cx="109.21608"
+          cy="121.23831"
+          r="8.6356897" />
+        <rect
+          class={`clock ${hue}-fill fillT`}
+          id="rect2461"
+          width="8.4906425"
+          height="2.225534"
+          x="104.97075"
+          y="110.21989" />
+        <rect
+          class={`clock ${hue}-fill fillT`}
+          id="rect2461-7"
+          width="8.4906425"
+          height="2.225534"
+          x="104.97076"
+          y="129.77719" />
+        <path
+          class={`stubbornClock ${hue}-stroke sT`}
+          d="m 108.39248,115.50054 v 6.50945"
+          id="path2667" />
+        <path
+          class={`stubbornClock ${hue}-stroke sT`}
+          d="m 108.44257,121.94957 4.75003,3.32155"
+          id="path2673" />
+      </g>
+    </svg>
+<style>
+  .stubbornClock {
+  fill: none;
+  stroke: transparent;
+  stroke-width: 2.76601;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-dasharray: none;
+  stroke-opacity: 1;
+  transition: stroke 0.325s linear;
+}
+.clock {
+  fill-opacity: 1;
+  stroke-width: 1.8;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-dasharray: none;
+  stroke-opacity: 1;
+  paint-order: normal;
+}
+.fillT {
+  fill: transparent;
+  transition: fill 0.325s linear;
+}
+.medium-stroke {
+  stroke: #6dc173;
+}
+.medium-fill {
+  fill: #6dc173;
+}
+
+</style>
---
+++ src/lib/TaskCard/TimerSpan/TimerSpan.svelte
@@ -0,0 +1,28 @@
+<script>
+  import ClockIcon from "../IconComponent/icons/ClockIcon.svelte"
+</script>
+<div class="topBar">
+  <ClockIcon/>
+  <div class="totalLength">
+    01:20:00
+  </div>
+</div>
+  <style>
+  .totalLength {
+  letter-spacing: -4px;
+  font-weight: bolder;
+  height: 80px;
+  padding-top: 1px;
+  padding-left: -2.5%;
+  font-size: 3rem;
+  transition: color 0.325s linear;
+  color: black;
+  user-select: none;
+  }
+  .topBar {
+  display: flex;
+  justify-content: left;
+  margin-left: 25px;
+  padding-top: 1%;
+}
+</style>
