036330b2b428e7a2d07e1740a8240967792d1ee0
Tati <iced.tati@gmail.com>
2024-06-04 15:24:14 -0400

    breakIcon

---
+++ src/lib/TaskCard/BreakIcon.svelte
@@ -0,0 +1,22 @@
+<svg
+class="fillT"
+xmlns="http://www.w3.org/2000/svg"
+viewBox="0 0 52 52"
+width="500"
+height="100px"
+>
+<line
+  x1="-98"
+  y1="25"
+  x2="148"
+  y2="25"
+  stroke-width="6"
+  stroke-linecap="round"
+  stroke-dasharray="10 10 200"
+  stroke="#9aa3b0" />
+</svg>
+
+<style>
+
+
+</style>
