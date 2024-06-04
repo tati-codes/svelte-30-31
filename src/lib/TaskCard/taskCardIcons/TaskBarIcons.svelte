c191635b2661bd780e3913b417ba1165bcc2f830
Tati <iced.tati@gmail.com>
2024-06-04 15:15:08 -0400

    MOVING

--- src/lib/TaskCard/taskCardIcons/TaskBarIcons.svelte
+++ src/lib/TaskCard/taskCardIcons/TaskBarIcons.svelte
@@ -3,24 +3,27 @@
   //18px left 4%
   //12px bottom
   //83px right 18%
-  import {IconRegistry} from "./taskCardIcons/IconRegistry";
+  import {IconRegistry} from "./IconRegistry";
+  export let width = 55, height= 55, x = 8, y = 5;
   export let type: TASKBAR_ITEM;
+  export let _class = "icon";
+  export let stroke = "white";
   let iconType = IconRegistry[type];
 </script>
 
 <svg
 xmlns="http://www.w3.org/2000/svg"
-width="55"
-height="55"
-x={8}
-y={5}
+{width}
+{height}
+{x}
+{y}
 viewBox="0 0 24 24"
 fill="none"
-stroke="white"
+{stroke}
 stroke-width="2"
 stroke-linecap="round"
 stroke-linejoin="round"
-class="icon"
+class={_class}
 >
 <svelte:component this={iconType}></svelte:component>
 </svg>
