e98f63966704b6e22efc0f934175de14c858b509
Tati <iced.tati@gmail.com>
2024-06-04 14:32:25 -0400

    movement

---
+++ src/lib/TaskCard/AddTaskCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Icon from "../Icons/Icon.svelte"
+
+// onClick={handleClick}
+
+</script>
+<svg
+xmlns="http://www.w3.org/2000/svg"
+version="1.1"
+viewBox="0 0 138 38"
+xml:space="preserve"
+width="490"
+class="addTaskCard fadeIn innerAdd"
+>
+<g class={"innerAdd"}>
+<Icon  x={60} y={9}/>
+  <path
+    class={`medium-gray-fill fillT innerAdd`}
+    stroke-width="0.265"
+    d="M3.532 9.05v19.426c.758 2.18 1.973 4.14 5.496 4.985l121.881-.226c2.946-.65 4.572-2.51 5.205-5.278l.073-19.16c-.494-2.822-2.342-4.415-4.953-5.313L9.051 3.412c-3.263.646-5.148 2.486-5.519 5.638z"
+  >
+  </path>
+</g>
+</svg>
+
+<!-- color={"gray"} -->
