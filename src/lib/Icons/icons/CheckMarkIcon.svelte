5bdba61beabd6a00caed461b460a36a08bab00bf
Tati <iced.tati@gmail.com>
2024-06-21 18:03:29 -0400

    fixing icon stuff

--- src/lib/Icons/icons/CheckMarkIcon.svelte
+++ src/lib/Icons/icons/CheckMarkIcon.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import Icon from "../Icon.svelte"
-  // export let x, y, width, height;`
+  export let x:number, y:number;
+  export let width = 55;
+  export let height = 55;
 </script>
 
-<Icon>
+<Icon {x} {y} {width} {height}>
   <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
 </Icon>
--- src/lib/TimerView/Timer/ActiveCircle.svelte
+++ src/lib/TimerView/Timer/ActiveCircle.svelte
@@ -8,3 +8,14 @@ cx="325"
 cy="325"
 stroke-width="90"
 ></circle>
+
+<style>
+  .innerCircle {
+  transform: rotate(45);
+  transition: all 1.325s linear;
+}
+.circle {
+  stroke: transparent;
+  transition: stroke 0.325s linear;
+}
+</style>
