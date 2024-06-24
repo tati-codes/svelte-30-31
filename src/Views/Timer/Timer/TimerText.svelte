e0b931560e9385b91abb38d5fe1c844e94c18242
Tati <iced.tati@gmail.com>
2024-06-24 17:39:02 -0400

    moving

---
+++ src/Views/Timer/Timer/TimerText.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+const isHour = true;
+let cLen = "00:00"
+let rem = "00:00"
+</script>
+
+<text
+letter-spacing="0"
+x="215"
+y={isHour ? "425" : "455"}
+class={`${isHour ? "hourTimerText" : "minuteTimerText"} dark-fill fillT fadeIn"`}
+>
+<tspan x="215" y={isHour ? "425" : "455"} stroke-width="0.265">
+  {cLen || rem}
+</tspan>
+</text>
+
+<style>
+  .minuteTimerText {
+  font-size: 125px;
+  word-spacing: 0%;
+  user-select: none;
+}
+.hourTimerText {
+  font-size: 95px;
+  word-spacing: 0%;
+  user-select: none;
+}
+</style>
