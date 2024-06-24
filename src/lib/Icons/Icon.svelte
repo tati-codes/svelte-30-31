ac4ee30bd7288708e128f26ea549dc9bef30269b
Tati <iced.tati@gmail.com>
2024-06-24 17:38:44 -0400

    new options

--- src/lib/Icons/Icon.svelte
+++ src/lib/Icons/Icon.svelte
@@ -3,6 +3,9 @@
   export let height = 55;
   export let x = 8;
   export let y = 5;
+  export let stroke = "white"
+  export let fill = "black"
+  export let classNames = ""
 </script>
 
 <svg
@@ -11,13 +14,13 @@ xmlns="http://www.w3.org/2000/svg"
 {height}
 {x}
 {y}
+{fill}
+{stroke}
 viewBox="0 0 500 500"
-fill="black"
-stroke="white"
 stroke-width="2"
 stroke-linecap="round"
 stroke-linejoin="round"
-class="_icon"
+class={"_icon " + classNames}
 >
 <slot/>
 </svg>
