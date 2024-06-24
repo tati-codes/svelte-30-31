ac2e3435b63abcbabb15ec69571d50f482fb7e9c
Tati <iced.tati@gmail.com>
2024-06-24 17:38:51 -0400

    cool button

---
+++ src/lib/Shared/BtnLabelColorAware.svelte
@@ -0,0 +1,16 @@
+<!-- export const BtnLabelColorAware = ({ label = "", x, y, size="48px"}: LabelProps) => { -->
+<script lang="ts">
+  export let x = 9, y = 50, size="25px", label: string;
+</script>
+
+<text
+  class="fadeIn light-fill fillT"
+  x={x}
+  y={y}
+  font-size={size}
+  font-weight="bolder"
+>
+  <tspan x={x} y={y} font-size={size}>
+    {label}
+  </tspan>
+</text>
