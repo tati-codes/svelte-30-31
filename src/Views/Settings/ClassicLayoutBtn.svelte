52081129b88afcb8f7b8c7589763925faecf5098
Tati <iced.tati@gmail.com>
2024-06-27 17:49:22 -0400

    fix

--- src/Views/Settings/ClassicLayoutBtn.svelte
+++ src/Views/Settings/ClassicLayoutBtn.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import BtnLabelColorAware from "../Shared/BtnLabelColorAware.svelte"
+  import BtnLabelColorAware from "../../lib/Shared/BtnLabelColorAware.svelte"
+
 </script>
 
 <BtnLabelColorAware label="CLASSIC" x={9} y={50} size="25px" />
