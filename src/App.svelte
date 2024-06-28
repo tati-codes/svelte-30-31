fa98d5c73445b04a849a0e74c1d2bf7e239cd229
Tati <iced.tati@gmail.com>
2024-06-28 14:30:38 -0400

    lol

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
-  import SettingsView from "./lib/SettingsView/SettingsView.svelte"
-import TimerView from "./Views/Timer/Timer.svelte"
+  import ColorStringChecker from "./Store/colorStringChecker.svelte"
+  import Settings from "./Views/Settings/Settings.svelte"
+  import TimerView from "./Views/Timer/TimerView.svelte"
+  import List from "./Views/List/List.svelte"
+  import {bg, light} from "./Store/color"
+  import {currentView} from "./Store/rootStore"
+  // "TIMER", "PICKER", "SETTINGS", "TASK_EDIT"
+  // $: cssVarStyles = Object.entries(styles)
+	// 	.map(([key, value]) => `--${key}:${value}`)
+	// 	.join(';');
+  // $: bsg = bg($light);
+  // style={bsg}
 </script>
 
-<main>
+<main class="bgt">
+{#if $currentView == "TIMER"}
+<List/>
+{:else if $currentView == "PICKER"}
 <TimerView/>
-<!-- <SettingsView/> -->
+{:else if $currentView == "SETTINGS"}
+  <Settings/>
+<!-- {:else if $currentView == "TASK_EDIT"} -->
+{/if}
 </main>
+
+<style>
+.bgt{
+  transition: background-color 0.325s linear;
+}
+</style>
