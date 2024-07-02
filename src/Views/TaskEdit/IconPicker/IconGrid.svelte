a9268aab424901ebabf1f10fd49becf88493cfbc
Tati <iced.tati@gmail.com>
2024-07-02 15:19:41 -0400

    icon picker component

---
+++ src/Views/TaskEdit/IconPicker/IconGrid.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { fill, bg, medium, light, stroke } from "../../../Store/color"
+  import { IconRegistry } from "../../Timer/TaskCard/taskCardIcons/IconRegistry"
+  import IconItem from "./IconItem.svelte"
+  import { editingTask } from "../../../Store/taskEdit";
+  import type { TASKBAR_ITEM } from "../../../../types"
+  import Icon from "../../../lib/Icons/Icon.svelte"
+
+  $: bsg = bg($medium);
+  $: lightstroke = stroke($light);
+  $: fillg = fill("transparent")
+  $: stg = `${lightstroke};${fillg}`
+  let keys: TASKBAR_ITEM[] = Object.keys(IconRegistry).filter(
+  (item) => item !== "close" && item !== "backspace" && item !== "add" 
+) as TASKBAR_ITEM[]
+</script>
+  
+  
+<div 
+style={bsg}
+class="iconPicker">
+{#each keys as key, i}
+  <IconItem selected={key == $editingTask?.icon} icon={key} change={() => null} tabIndex={i+2}>
+    <Icon style={stg}  width={18} height={18} viewbox="0 0 24 24">
+      <svelte:component this={IconRegistry[key]}/>
+    </Icon>
+  </IconItem>
+{/each}
+</div>
+
+<style>
+  .iconPicker {
+  z-index: 1;
+  height: 400px;
+  width: 480px;
+  margin-left: 25px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  }
+
+</style>
---
+++ src/Views/TaskEdit/IconPicker/IconItem.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { TASKBAR_ITEM } from "../../../../types"
+
+export let selected: boolean, icon: TASKBAR_ITEM, change: Function, tabIndex: number;
+</script>
+
+<label
+class={`iconItem  ${selected ? "selectedIcon" : ""} fadeIn`}
+role="tab"
+{tabIndex}
+for={`${icon}-icon`}
+on:click={change}
+>
+<slot/>
+</label>
+
+<style>
+  .iconItem {
+  text-align: center;
+  justify-content: center;
+  display: grid;
+  background-color: rgba(0, 0, 0, 0);
+  -webkit-transition: 0.2s -webkit-filter linear;
+  -moz-transition: 0.2s -moz-filter linear;
+  -moz-transition: 0.2s filter linear;
+  transition: 0.2s all linear;
+  user-select: none;
+  transform: translateY(30%);
+}
+
+:global(.iconItem > svg) {
+  transform: scale(2.5);
+}
+</style>
