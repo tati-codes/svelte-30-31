7f8bc9936f70e990b0ce4c14756fb7dea23596c0
Tati <iced.tati@gmail.com>
2024-07-02 18:27:14 -0400

    time qworking

--- src/Views/TaskEdit/TimePicker/NumpadBtn.svelte
+++ src/Views/TaskEdit/TimePicker/NumpadBtn.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import { bg, medium } from "../../../Store/color"
-  export let label: string, cb: Function
+  export let label: number, cb: Function
   $: bsg = bg($medium);
+  let offset = label == 0 ? 13 : 3
 </script>
 
 <li
 style={bsg}
-aria-selected="true"
-tabindex={0}
+tabindex={label+offset}
 class="numpadItem"
 on:click={() => cb()}
 >
--- src/Views/TaskEdit/TimePicker/TimePicker.svelte
+++ src/Views/TaskEdit/TimePicker/TimePicker.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-  import { seconds_to_hhmmss } from "../../../lib/Shared/time"
-  import { editingTask } from "../../../Store/taskEdit"
+  import { handleTimePickerChange } from "../../../Store/actions/taskEdit/handleTimePickerChange"
   import BackspaceBtn from "./BackspaceBtn.svelte"
   import CheckmarkBtn from "./CheckmarkBtn.svelte"
   import NumpadBtn from "./NumpadBtn.svelte"
+  export let len:string;
   const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, "checkmark", 0, "backspace"]
-  $: len = seconds_to_hhmmss($editingTask!.length)
-
-  let handleChange = (num: number) => {
+  export let handleChange = (num: number) => {
     let currentLen: string[] = len.replaceAll(":", "").split("")
     if (num > -1 && num <= 9) {
       currentLen.shift()
@@ -18,22 +16,22 @@
     }
     currentLen.splice(2, 0, ":")
     currentLen.splice(5, 0, ":")
-    //i don't even know
-    let currentLen2: string[] = currentLen.join("").replaceAll(":", "").split("")
-    let hours = Number(currentLen2.slice(0, 2).join("")) * 3600
-    let minutes = Number(currentLen2.slice(2, 4).join("")) * 60
-    let seconds = Number(currentLen2.slice(4, 6).join(""))
-    let total = hours + minutes + seconds
-    if (total < 60) seconds = 60
-    else if (total >= 86400) total = 86399 //1 day
-    // sendChange({ length: total, remaining_seconds: total })
+    len = currentLen.join("")
+    handleTimePickerChange(len);
   }
+  export const onNumPress = (e: KeyboardEvent) => {
+    console.log(e.key)
+    let nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    if (nums.includes(e.key)) {
+      handleChange(Number(e.key))
+    } 
+}
 </script>
 
-<ul class="numpad fadeIn">
+<ul class="numpad fadeIn" on:keypress={onNumPress}>
   {#each labels as item}
     {#if typeof item == "number"}
-      <NumpadBtn label={item.toString()} cb={() => handleChange(item)}/>
+      <NumpadBtn label={item} cb={() => handleChange(item)}/>
     {:else if item == "backspace"}
       <BackspaceBtn cb={() => handleChange(-1)}/>
     {:else if  item == "checkmark"}
