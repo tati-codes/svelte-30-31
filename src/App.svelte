f9c87970fd96a3c4a0fe4669b759fb64d78c7d62
Tati <iced.tati@gmail.com>
2024-07-07 14:17:42 -0400

    debug stuff

--- src/App.svelte
+++ src/App.svelte
@@ -4,30 +4,54 @@
   import TimerView from "./Views/Timer/TimerView.svelte"
   import List from "./Views/List/List.svelte"
   import {bg, light} from "./Store/color"
-  import {currentTaskList, currentView, root} from "./Store/rootStore"
+  import {currentTask, currentTaskList, currentView, root} from "./Store/rootStore"
   import TaskEdit from "./Views/TaskEdit/TaskEdit.svelte"
+  
   // "TIMER", "PICKER", "SETTINGS", "TASK_EDIT"
   // $: cssVarStyles = Object.entries(styles)
 	// 	.map(([key, value]) => `--${key}:${value}`)
 	// 	.join(';');
   import { setEditID } from "./Store/taskEdit"
+  import DebugStore from "./lib/Shared/debugStore.svelte"
   setEditID($currentTaskList.tasks[0].id);
 </script>
 
-<main class="bgt">
-{#if $currentView == "TIMER"}
-<List/>
-{:else if $currentView == "PICKER"}
-<TimerView/>
-{:else if $currentView == "SETTINGS"}
-<Settings/>
-{:else if $currentView == "TASK_EDIT"}
-<TaskEdit/>
-{/if}
-</main>
+<div class="container">
 
+  <main class="bgt">
+    {#if $currentView == "TIMER"}
+    <TimerView/>
+    {:else if $currentView == "PICKER"}
+    <List/>
+    {:else if $currentView == "SETTINGS"}
+    <Settings/>
+    {:else if $currentView == "TASK_EDIT"}
+    <TaskEdit/>
+    {/if}
+  </main>
+    <aside>
+      <DebugStore obj={$root} title="root"/>
+      <DebugStore obj={$currentTaskList} title="tasklist"/>
+      <DebugStore obj={$currentTask} title="task"/>
+    </aside>
+</div>
 <style>
+
+  aside {
+    background: rgba(0, 0, 0, 0.8);
+    margin: 20px;
+    padding: 20px;
+    z-index: 0;
+    position: absolute;
+    margin-left: 50%;
+    font-family: Helvetica;
+  }
 .bgt{
   transition: background-color 0.325s linear;
+}
+.container {
+  display: grid; 
+  grid-template-columns: 5fr 5fr; 
+  gap: 0px 0px; 
 }
 </style>
---
+++ src/lib/Shared/debugStore.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { isPrimitive } from "./isPrimitive"
+  export let obj, title;
+  console.log(obj)
+  let keys = Object.keys(obj)
+  let values = Object.values(obj)
+</script>
+<div>
+  <h2>{title}</h2>
+  {#each keys as key, i}
+    {#if isPrimitive(values[i])}
+      <p>
+        <strong>{key}</strong>: {values[i]}
+      </p>
+    {/if}
+  {/each}
+</div>
+
+<style>
+  strong {
+  font-weight: bolder;
+}
+  h2 {
+    text-decoration:underline;
+  }
+
+  div {
+  border-top: 1px dashed #aaa;
+  padding: 0.5em 0.5em 0;
+}
+
+  div {
+  padding: 5px;
+  margin: 5px;
+  border: 2px dashed mus;
+}
+
+  summary { 
+  font-weight: bold;
+  margin: -0.5em -0.5em 0;
+  padding: 0.5em;
+}
+
+details[open] {
+  padding: 0.5em;
+}
+
+details[open] summary {
+  border-bottom: 1px solid #aaa;
+  margin-bottom: 0.5em;
+}
+
+</style>
