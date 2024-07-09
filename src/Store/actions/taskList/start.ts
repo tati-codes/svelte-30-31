79c2600fa16131af95eeeb304e4768cef70f680c
Tati <iced.tati@gmail.com>
2024-07-09 16:07:22 -0400

    see isnt this so much cleaner

--- src/Store/actions/taskList/start.ts
+++ src/Store/actions/taskList/start.ts
@@ -4,25 +4,11 @@ import { set_timer } from "./set_timer";
 
 export const start = () => root.update($root => {
   let taskList = $root.taskLists[$root.selected]
-  let currentTask = taskList.tasks[0]
-  if (taskList.status == "TIMER_ACTIVE" && currentTask.status === "TICKING") return $root
+  if (taskList.status == "TIMER_ACTIVE" && taskList.timer) return $root
   else {
     let start = moment([])
-    currentTask.status = "TICKING"
     taskList.status = "TIMER_ACTIVE"
-    currentTask.start_tick = start
-    currentTask.computed = [start, start.clone().add(currentTask.remaining_seconds, "seconds")]
-    
-    //computing times for whole taskList
-    let [_, rollingStart] = currentTask.computed
-    taskList.tasks.forEach(task => {
-      if (task.id === currentTask.id) return
-      let computing: [moment.Moment, moment.Moment] = [rollingStart.clone(), rollingStart.clone().add(task.remaining_seconds, "seconds")]
-      task.computed = computing
-      rollingStart = computing[1].clone()
-    })
-
-    //set timer
+    taskList.start_tick = start
     taskList.timer = set_timer()
     return $root
   }
