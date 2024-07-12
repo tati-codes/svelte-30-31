3cb7834e8e1fe8b429c394b9ce5aabab5b63f3f5
Tati <iced.tati@gmail.com>
2024-07-11 21:51:13 -0400

    name fixes | forgotten icon

--- src/Store/actions/taskList/add.ts
+++ src/Store/actions/taskList/add.ts
@@ -1,7 +1,7 @@
 import type { Task } from "../../Task";
 import { root } from "../../rootStore";
 
-export const end = (task: Task) => root.update($root => {
+export const addTask = (task: Task) => root.update($root => {
   let taskList = $root.taskLists[$root.selected]
   let break_index = taskList.tasks.findIndex((task) => task.name === "_BREAK") 
   taskList.tasks.splice(break_index, 0, task)
