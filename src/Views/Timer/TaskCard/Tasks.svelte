243aa404706c16c4d9cf36c9d72e46e0cd86d13b
Tati <iced.tati@gmail.com>
2024-07-22 20:14:24 -0400

    that was just here

--- src/Views/Timer/TaskCard/Tasks.svelte
+++ src/Views/Timer/TaskCard/Tasks.svelte
@@ -19,4 +19,28 @@
   {/each}
   <AddTaskCard/>
 </div>
+
+<style>
+  .card{
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    margin-left: 20%;
+  }
+  :global(.card > .lineStroke) {
+    margin: -10%
   
+  }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
--- src/Views/Timer/TimerView.svelte
+++ src/Views/Timer/TimerView.svelte
@@ -9,27 +9,3 @@
 <Timer/>
 <Tasks/>
 
-<style>
-  .card{
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    margin-left: 20%;
-  }
-  :global(.card > .lineStroke) {
-    margin: -10%
-  
-  }
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-</style>
