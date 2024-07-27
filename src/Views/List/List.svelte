8ccd7f70a099cb1cbd5ccdfab95b2e905475c405
Tati <iced.tati@gmail.com>
2024-07-27 00:53:59 -0400

    styling

--- src/Views/List/List.svelte
+++ src/Views/List/List.svelte
@@ -1,28 +1,35 @@
 <script lang="ts">
+  import { dark, bg } from "../../Store/color"
+  import { tasklists } from "../../Store/rootStore"
   import AddListBtn from "./AddListBtn.svelte"
-
-
+  import PickerCard from "./PickerCard.svelte"
+  import TopBarTaskListPicker from "./TopBarTaskListPicker.svelte"
   // let { taskLists, dispatch } = useTaskListPickerStore((state) => state)
   // let select = (id: string) => () => (dispatch("select", id), swap())
   // let del = (id: string) => () => dispatch("delete", {id, changes: null})
-  let tasklists: any[] = [];
-
+  
 </script>
 
-<div class="pickerView dark-background fadeIn">
-  {#each tasklists as list}
-    <!-- <PickerCard name={list.name} select={list.id} del={del(tasklist.id)} /> -->
+<TopBarTaskListPicker/>
+<div class="pickerView dark-background fadeIn"  style={bg($dark)}>
+  {#each $tasklists as list, i}
+    <PickerCard name={list.name} index={i} />
   {/each}
     <AddListBtn/>
 </div>
 
 <style>
-  
 .pickerView {
   border-radius: 10px;
   text-align: center;
   max-width: 465px;
   margin-left: 24px;
   margin-top: 25px;
+  padding-top: 25px;
+  display:flex;
+  flex-direction: column;
+  gap: 25px;
+  padding-bottom: 25px;
 }
+
 </style>
---
+++ src/Views/List/TopBarTaskListPicker.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import {bg, medium } from "../../Store/color"
+  import SettingsButton from "../Timer/TopBar/SettingsButton.svelte"
+  import TimerListButton from "../Timer/TopBar/TimerListButton.svelte"
+
+</script>
+
+
+
+<div class="topBar">
+  <TimerListButton/>
+  <div style={bg($medium)} class="tName bgT fadeIn" >
+    <span>Select your tasklist</span>
+  </div>
+    <SettingsButton/>
+</div>
+
+<style>
+.topBar {
+  display: flex;
+  justify-content: left;
+  margin-left: 25px;
+  padding-top: 1%;
+}
+
+.tName {
+  height: 80px;
+  width: 420px;
+
+  padding-right: 1%;
+  padding-left: 1%;
+  font-size: 3rem;
+  margin-right: 1%;
+  margin-left: 1%;
+  overflow: hidden;
+  user-select: none;
+}
+</style>
