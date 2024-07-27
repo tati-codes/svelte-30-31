148cd176dc54722f7be6bc232c6d25ff1331638d
Tati <iced.tati@gmail.com>
2024-07-27 00:52:31 -0400

    minor

--- src/Store/Tasklist.ts
+++ src/Store/Tasklist.ts
@@ -42,4 +42,4 @@ export class TaskList implements TaskListI {
 //   }
 // }
 
-export let defaultTaskList: () => TaskList = () => new TaskList("Classic Pomo", [new Task("default", 90, "basket"), new Task("different", 1, "book")])
+export let defaultTaskList: () => TaskList = () => new TaskList("Classic Pomo", [new Task("default", 5, "basket"), new Task("different", 1, "book")])
