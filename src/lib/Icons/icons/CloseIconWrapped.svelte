e12b5a71d4053b04c25a1f46cf5d8f069a380192
Tati <iced.tati@gmail.com>
2024-07-27 00:50:56 -0400

    icocn fixws

---
+++ src/lib/Icons/icons/CloseIconWrapped.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { light } from "../../../Store/color"
+  import CloseIcon from "../../../Views/Timer/TaskCard/taskCardIcons/CloseIcon.svelte"
+  import Icon from "../Icon.svelte"
+  export let width = 55;
+  export let height = 55;
+  export let x = 8;
+  export let y = 5;
+</script>
+
+  <Icon x={x} y={y} width={48} height={48} viewbox="0 0 24 24" classNames="deleteIcon">
+    <rect  fill="transparent" stroke-width={"1px"} x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+    <line  x1="9" y1="9" x2="15" y2="15"></line>
+    <line  x1="15" y1="9" x2="9" y2="15"></line>
+  </Icon>
+
+
+  <!-- stroke={$light}
+stroke={$light}
+stroke={$light} -->
