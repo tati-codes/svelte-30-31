2c39cb2334f1bf5f09ff09583fa8527bb0d1f6c5
Tati <iced.tati@gmail.com>
2024-07-27 00:50:23 -0400

    svg to div, also pretty

--- src/Views/List/AddListBtn.svelte
+++ src/Views/List/AddListBtn.svelte
@@ -1,28 +1,80 @@
 <script lang="ts">
   import Icon from "../../lib/Icons/Icon.svelte"
+  import { bg, combineStyles, dark, light, medium } from "../../Store/color"
   import AddIcon from "../Timer/TaskCard/taskCardIcons/AddIcon.svelte"
+  import TaskBarIcons from "../Timer/TaskCard/taskCardIcons/TaskBarIcons.svelte"
 
-  let add = () => null
 </script>
-<!-- let add = useTaskListPickerStore(state => () => state.dispatch("addTaskList", null)) -->
-  
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  viewBox="0 0 138 38"
-  width="450"
-  class="svgTaskCard innerAdd"
-  on:click={null}
-  transform="scale(0.77)"
->
-  <g>
-    <path
-      class="dark-fill fillT"
-      stroke-width="0.265"
-      d="M3.532 9.05v19.426c.758 2.18 1.973 4.14 5.496 4.985l121.881-.226c2.946-.65 4.572-2.51 5.205-5.278l.073-19.16c-.494-2.822-2.342-4.415-4.953-5.313L9.051 3.412c-3.263.646-5.148 2.486-5.519 5.638z"
-    ></path>
-    <Icon x={60} y={9}>
-      <AddIcon/>
-    </Icon>
-  </g>
-</svg>
+
+<div style={combineStyles(bg($dark),`border: 2px solid ${$medium};`)} class="taskListCard fillT" on:click={() => console.log("add")} >
+  <Icon x={0} y={0} width={48} height={48} viewbox={"0 0 24 24"} classNames="addIcon">
+    <rect fill={"transparent"} x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+    <line x1="12" y1="8" x2="12" y2="16"></line>
+    <line  x1="8" y1="12" x2="16" y2="12"></line>
+  </Icon>
+</div>
+
+<style>
+  .taskListCard {
+    
+    font-size: 1em;
+    font-weight: 500;
+    margin: auto;
+    font-family: inherit;
+    cursor: pointer;
+    transition: 1s border-color linear;
+    transition: 0.27s filter linear;
+    display: flex;
+    width: 400px;
+    height: 105px;
+    border-radius: 25px;
+    display: flex;
+    flex-direction: row;
+  }
+  :global(.taskListCard > .addIcon) {
+margin:auto;
+    font-size: 2rem;
+  }
+  div.taskListCard:hover {
+    animation: fadeinout 0.7s linear forwards;
+    filter: brightness(90%);
+  }
+
+  :global(.taskListCard > .addIcon) {
+    rotate: 0;
+    stroke: white;
+    opacity: 30%;
+    transition: filter 0.325s linear, rotate 0.325s linear, stroke 0.125s linear, opacity 0.1s linear;
+
+  }
+  :global(.taskListCard:hover > .addIcon) {
+    /* animation: big 1s linear both; */
+    rotate: 90deg;
+    scale: 1.1;
+    opacity:1;
+    transition: filter 0.325s linear, rotate 0.225s linear, stroke 0.125s linear, opacity 0.5s linear;
+  }
+  :global(.addIcon:active) {
+    stroke: greenyellow;
+    transition: filter 0.325s linear, rotate 0.325s linear, stroke 0.125s linear, opacity 0.5s linear;
+  }
+@keyframes big {
+    100% { 
+      rotate: 90deg;
+      scale: 1.2;
+      opacity: 1;
+    }
+  }
+
+@keyframes fadeinout {
+  100% { border: 2px solid transparent; }
+}
+@keyframes in {
+  100% { opacity: 1; }
+}
+  div.taskListCard:focus,
+  div.taskListCard:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+
+</style>
