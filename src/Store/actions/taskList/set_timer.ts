f5578493c27aebb69bbdac408341a72e8c65f23a
Tati <iced.tati@gmail.com>
2024-07-27 00:51:19 -0400

    bugs with Timer

--- src/Store/actions/taskList/set_timer.ts
+++ src/Store/actions/taskList/set_timer.ts
@@ -1,11 +1,11 @@
 import { get } from "svelte/store"
 import { currentTask } from "../../rootStore"
 import { tick } from "../task/tick"
-import { sendToBottom } from "./sendBottom"
+import { markDone } from "./markDone"
 
 export let set_timer = () => setInterval(() => {
     let {remaining_seconds} = get(currentTask)
     if (remaining_seconds > 0) tick()
     else if (remaining_seconds < 0) console.error("timer error")
-    else if (remaining_seconds <= 0) sendToBottom()
+    else if (remaining_seconds <= 0) markDone()
   }, 1000)
