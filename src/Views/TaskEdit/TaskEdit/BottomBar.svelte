e09b84f5106e300a75f8db596aefab3f6149dd93
Tati <iced.tati@gmail.com>
2024-07-29 13:57:21 -0400

    fixed the fixme bug about what delete should do, also added an animation

--- src/Views/TaskEdit/TaskEdit/BottomBar.svelte
+++ src/Views/TaskEdit/TaskEdit/BottomBar.svelte
@@ -8,15 +8,13 @@
   import { bg, medium } from "../../../Store/color"
   import { editingTask } from "../../../Store/taskEdit"
   $: bsg = bg($medium);
-  const handleGoBackKey = onEnter(() => setView("TIMER"))
-  const handleDeleteKey = onEnter(() => deleteTask($editingTask?.id || "missing"))
-//  on:click={swap}
-// on:click={() => (swap(), dispatch("delete", {id, changes: null}))}
-
-//swap should set rootStore view to TIMER
-//also send delete to taskListstore
-//FIXME
-//delete should bring you back to timer too
+  const doDelete = () => {
+    deleteTask($editingTask?.id || "missing")
+    setView("TIMER")
+  }
+  const doBack = () => setView("TIMER")
+  const handleGoBackKey = onEnter(doBack)
+  const handleDeleteKey = onEnter(doDelete)
 </script>
 <div class="bottomBar">
   <li class="tab-li marginright" >
@@ -26,7 +24,7 @@
       role="tab"
       aria-selected="true"
       aria-controls={"icon-panel"}
-      on:click={() => deleteTask($editingTask?.id || "missing")}
+      on:click={doDelete}
       on:keypress={handleDeleteKey}
       tabIndex={60}
     >
@@ -41,7 +39,7 @@
       aria-selected="true"
       aria-controls={"icon-panel"}
       tabIndex={61}
-      on:click={() => setView("TIMER")}
+      on:click={doBack}
       on:keypress={handleGoBackKey}
     >
       <ReplySolidIcon x={0} y={0} height={35}/>
@@ -64,4 +62,16 @@
     transition: 0.2s filter linear;
     transition: all 0.2s linear;
   }
+  .background-darken {
+  background-color: rgba(0, 0, 0, 0);
+  -webkit-transition: 0.2s -webkit-filter linear;
+  -moz-transition: 0.2s -moz-filter linear;
+  -moz-transition: 0.2s filter linear;
+  transition: 0.2s filter linear;
+  transition: 0.2s all linear;
+}
+  .background-darken:hover {
+  filter: brightness(120%);
+  box-shadow: -2px 2px 2px rgba(0,0,0,0.8);
+}
   </style>
