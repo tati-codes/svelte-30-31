fe717ad8974812244e3c1d15a25685912d25484b
Tati <iced.tati@gmail.com>
2024-07-29 14:38:01 -0400

    HOISTED CSS

--- src/Store/actions/task/addTime.ts
+++ src/Store/actions/task/addTime.ts
@@ -2,11 +2,11 @@ import { add_seconds } from "../../../lib/Shared/add_remove_seconds";
 import { root } from "../../rootStore";
 
 export let addTime = () => root.update(($root) => {
-  let {taskLists, selected} = $root
-  let task = taskLists[selected].tasks[0]
+  let taskList = $root.taskLists.find(task => task.id == $root.selectedId)!;
+  let task = taskList.tasks[0]
   let potential = add_seconds(task.remaining_seconds)
-  let taskLength = taskLists[selected].tasks[0].length
+  let taskLength = task.length
   if (potential > taskLength) potential = taskLength
-  taskLists[selected].tasks[0].remaining_seconds = potential
+  task.remaining_seconds = potential
   return $root
 });
