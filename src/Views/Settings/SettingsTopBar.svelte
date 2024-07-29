836db9725b05d8935721f9b085d8721eebfb1b25
Tati <iced.tati@gmail.com>
2024-07-29 19:58:15 -0400

    made it so clicking on settings top bar or settings btn while in settings it lets you go back to timer

--- src/Views/Settings/SettingsTopBar.svelte
+++ src/Views/Settings/SettingsTopBar.svelte
@@ -2,13 +2,15 @@
   import {bg, medium } from "../../Store/color"
   import GoSettings from "../Timer/TopBar/GoSettings.svelte"
   import GoListPicker from "../Timer/TopBar/GoListPicker.svelte"
+  import { setView } from "../../Store/actions/root/setView"
+  let goTimer = () => setView("TIMER")
 </script>
 
 
 
 <div class="topBar">
   <GoListPicker/>
-  <div style={bg($medium)} class="tName bgT fadeIn" >
+  <div style={bg($medium)} class="tName bgT fadeIn" on:click={goTimer}>
     <span>Settings</span>
   </div>
     <GoSettings/>
--- src/Views/Timer/TopBar/GoSettings.svelte
+++ src/Views/Timer/TopBar/GoSettings.svelte
@@ -3,10 +3,13 @@
   import SettingsBtn from "../../../lib/Shared/SettingsBtn.svelte"
   import { setView } from "../../../Store/actions/root/setView"
   import { light } from "../../../Store/color";
+  import { currentView } from "../../../Store/settingStore"
+  $: isSettings = $currentView === "SETTINGS"
   const goSettings = () => setView("SETTINGS");
+  const goTimer = () => setView("TIMER")
 
 </script>
 
-<SettingsBtn cb={goSettings}>
+<SettingsBtn cb={isSettings ? goTimer : goSettings}>
   <GearIcon fill={$light}/>
 </SettingsBtn>
