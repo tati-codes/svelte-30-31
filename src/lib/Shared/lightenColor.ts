05d9c0fb78c1d7edc17912ed2f3f5469389f7c87
Tati <iced.tati@gmail.com>
2024-07-30 19:08:30 -0400

    minor

--- src/lib/Shared/lightenColor.ts
+++ src/lib/Shared/lightenColor.ts
@@ -1,6 +1,5 @@
 //from https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
 export let LightenDarkenColor = (col:string , amt: number) => {
-  console.log(col.substring(1).toUpperCase())
   var num = parseInt(col.substring(1).toUpperCase(), 16);
   var r = (num >> 16) + amt;
   var b = ((num >> 8) & 0x00FF) + amt;
