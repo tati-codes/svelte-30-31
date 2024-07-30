fbd64e32f2b9a799a4dbfd7dbe6ea3ca5593ff3b
Tati <iced.tati@gmail.com>
2024-07-30 18:47:22 -0400

    bug when changing tasklists

--- src/Views/Timer/TopBar/GoListPicker.svelte
+++ src/Views/Timer/TopBar/GoListPicker.svelte
@@ -2,8 +2,15 @@
   import FolderIcon from "../../../lib/Icons/icons/FolderIcon.svelte"
   import SettingsBtn from "../../../lib/Shared/SettingsBtn.svelte"
   import { setView } from "../../../Store/actions/root/setView"
+  import { pause } from "../../../Store/actions/task/pause"
   import { light } from "../../../Store/color";
-  const goPicker = () => setView("PICKER");
+  import { currentTaskList } from "../../../Store/rootStore"
+  const goPicker = () => { 
+    if ($currentTaskList.isPlaying) {
+      pause();
+    }
+    setView("PICKER");
+  }
 
 </script>
 
