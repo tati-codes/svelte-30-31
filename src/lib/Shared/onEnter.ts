9ad20511abba42594129c0c7ab0a3136eff8a41b
Tati <iced.tati@gmail.com>
2024-08-29 20:38:26 -0400

    made onEnter stop propagation in the handler instead of the callback

--- src/lib/Shared/onEnter.ts
+++ src/lib/Shared/onEnter.ts
@@ -1,5 +1,6 @@
 export const onEnter = (curried: Function) => {
     return (e: KeyboardEvent) => {
+      e.stopPropagation()
       if (e.key == "Enter" || e.key == " ") {
         curried();
     }
