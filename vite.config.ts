60c8c9602d9ead62b235f685a63dd981a34da6d5
Tati <iced.tati@gmail.com>
2024-09-02 17:04:06 -0400

    fixed vite output

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [svelte(),
     VitePWA({
       scope:"/timer2/",
