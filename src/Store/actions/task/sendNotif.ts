f92b0d6cedca8eb517f60064b5dc4748bd1e8f2b
Tati <iced.tati@gmail.com>
2024-09-02 23:01:06 -0400

    lol bug

--- src/Store/actions/task/sendNotif.ts
+++ src/Store/actions/task/sendNotif.ts
@@ -6,5 +6,5 @@ import { root } from "../../rootStore"
 export const sendNotif = (taskName: string, message: string) => {
   if (window.Notification.permission !== "granted") return
   else if (get(root).notificationsPermissions === false) return
-  else new Notification(`${taskName} {message}!`)
+  else new Notification(`${taskName} ${message}!`)
 }
