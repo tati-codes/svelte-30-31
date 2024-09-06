a89d7f694be5acf2e72e2c8304f350782aafe0a8
Tati <iced.tati@gmail.com>
2024-09-06 17:56:09 -0400

    why is deleting pausing the timer

--- src/Store/actions/taskEdit/deleteTask.ts
+++ src/Store/actions/taskEdit/deleteTask.ts
@@ -11,7 +11,7 @@ export const deleteTask = (id: string) => root.update($root => {
   let tasklist = $root.taskLists.find(task => task.id == $root.selectedId)!;
   let tasks = tasklist.tasks
   if (tasks.length < 3) return $root;
-  if (tasklist.timer) clearInterval(tasklist.timer)
+  // if (tasklist.timer) clearInterval(tasklist.timer)
   let taskIndex = tasks.findIndex(task => task.id === id)
   tasks.splice(taskIndex, 1)
   return $root
